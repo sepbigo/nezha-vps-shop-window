e143fd6673c6b8b57edf5e0a24a375ceea23fbf5
bmqy <bmqy@qq.com>
2025-03-07 08:59:16 +0800

    init

--- src/components/RemainingPrice.vue
+++ src/components/RemainingPrice.vue
@@ -5,7 +5,8 @@
         <div class="flex flex-row text-primary/80 drop-shadow justify-start items-center">
           <span :class="cn(
             'font-money tracking-widest relative block -mt-10',
-            `after:absolute after:block after:content-[''] after:-bottom-1 after:-left-1 after:-right-1 z-10 after:h-1 after:-z-10 after:border-primary after:border-t after:border-b after:-rotate-2`,
+            `before:absolute before:block before:content-[''] before:-bottom-1 before:-left-1 before:-right-1 z-10 before:h-0 before:-z-10 before:border-primary before:border-b before:-rotate-183`,
+            `after:absolute after:block after:content-[''] after:-bottom-2 after:-left-1 after:-right-1 z-10 after:h-0 after:-z-10 after:border-primary after:border-b after:-rotate-2`,
           )">{{ ramainingData.remainingPrice }}{{ ramainingData.priceUnit }}
           </span>
         </div>
@@ -18,10 +19,10 @@
 </template>
 <script setup>
 import {
-Tooltip,
-TooltipContent,
-TooltipProvider,
-TooltipTrigger
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
 } from '@/components/shadcn/ui/tooltip';
 import { parseRemainingData } from '@/lib/function';
 import { cn } from '@/lib/utils';
